1c2b3790ccf47ca218fea90c293b14d965b3f603
Rye Nguyen <ryenguyen7411@gmail.com>
2023-04-18 23:05:01 +0700

    feat: upgrade pnpm to compatible version

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import qrcode from './assets/images/image-qr-code.png';
+
 function App() {
   return (
     <div className="container">
       <div className="card">
-        <img src="/image-qr-code.png" alt="qr code" />
+        <img src={qrcode} alt="qr code" />
         <h3>Improve your front-end skills by building projects</h3>
         <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
       </div>
